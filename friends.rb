f687d95e44c3dd002aae2e48f182b4e70e545b4a
Nick Sacco <nicksacco10@gmail.com>
2022-12-07 16:32:27 -0800

    Add elsif statement

--- friends.rb
+++ friends.rb
@@ -1,7 +1,10 @@
 best_friend = "Ted"
+good_friend = "Bill"
 
 if best_friend == "Bill"
     puts "Hey Billy!"
+elsif good_friend == "Bill"
+    puts "Yo it's Bill!"
 else
     puts "Nice to meet you #{best_friend}"
 end
